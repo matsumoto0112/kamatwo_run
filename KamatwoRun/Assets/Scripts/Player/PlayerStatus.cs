a7c81e26407c1c71743524bb9c849f33e2af598b
Dem4236 <onoderadesk0402@gmail.com>
2021-01-29 09:52:19 +0900

    [feat] スコアオブジェクトに衝突時削除関数を呼ぶ

--- KamatwoRun/Assets/Scripts/Player/PlayerStatus.cs
+++ KamatwoRun/Assets/Scripts/Player/PlayerStatus.cs
@@ -4,4 +4,20 @@ using UnityEngine;
 
 public class PlayerStatus : CharacterComponent
 {
+    public int score { get; private set; }
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+        score = 0;
+    }
+
+    /// <summary>
+    /// ÉXÉRÉAâ¡éZ
+    /// </summary>
+    /// <param name="score"></param>
+    public void AddScore(int score)
+    {
+        this.score += score;
+    }
 }

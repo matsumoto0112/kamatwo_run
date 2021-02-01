9fcad5a37b9a4881468a6650cb395f4b778ba3f6
matsumoto <matsumoto.k017a1377@gmail.com>
2021-02-01 14:27:49 +0900

    [fix] リザルトシーンにランキングボードを置いた

--- KamatwoRun/Assets/Scripts/Results/RankingBoard.cs
+++ KamatwoRun/Assets/Scripts/Results/RankingBoard.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
 
+/// <summary>
+/// ランキングの種類
+/// </summary>
+public enum RankingType
+{
+    Weekday,
+    Holiday,
+    PlayedGameMode,
+}
+
 public class RankingBoard : MonoBehaviour
 {
+    [SerializeField, Header("ランキングの種類")]
+    private RankingType type;
     [SerializeField, Header("モード名を表示するテキスト")]
     private Text modeText;
     [SerializeField]
@@ -16,11 +28,21 @@ public class RankingBoard : MonoBehaviour
         //セーブ処理をしてプレイデータをアップロードしておく
         //セーブ後にはデータがリセットされるため、複数回呼んでもデータが破壊されることはない
         GameDataStore.Instance.SaveGameData();
-        RankingData data = GameDataStore.Instance.GetSavedRankingData();
+        RankingData data; if (type == RankingType.PlayedGameMode)
+        {
+
+            data = GameDataStore.Instance.GetSavedRankingData();
+            modeText.text = GameDataStore.Instance.PlayedMode.PlayModeText();
+        }
+        else
+        {
+            PlayMode mode = type == RankingType.Weekday ? PlayMode.Weekday : PlayMode.Holiday;
+            data = GameDataStore.Instance.GetSavedRankingData(mode);
+            modeText.text = mode.PlayModeText();
+        }
 
         Assert.IsTrue(data.playerDatas.Length == startPoints.Count, "リザルトの要素数とUIの要素数が一致しません");
 
-        modeText.text = GameDataStore.Instance.PlayedMode.PlayModeText();
         for (int i = 0; i < data.playerDatas.Length; i++)
         {
             startPoints[i].text = $"{ data.playerDatas[i].score}";
